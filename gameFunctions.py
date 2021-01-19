33f7a3e0acf26a503ad44f83f571ebe4901a6dad
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2021-01-19 18:35:55 +0100

    Add files via upload

---
+++ gameFunctions.py
@@ -0,0 +1,57 @@
+import ast
+import random
+
+
+
+def addPlayer(name):
+    with open('players.txt', 'r') as f:
+        data = f.read()
+        upd = ast.literal_eval(data)
+        upd[name] = 50
+        with open('players.txt', 'w') as f:
+            f.write(str(upd)+'\n')
+
+def addPoints(name, amount):
+    with open('players.txt', 'r') as f:
+        data = f.read()
+        upd = ast.literal_eval(data)
+        upd[name] += amount
+        with open('players.txt', 'w') as f:
+            f.write(str(upd))
+
+def removePoints(name, amount):
+    with open('players.txt', 'r') as f:
+        data = f.read()
+        upd = ast.literal_eval(data)
+        upd[name] -= amount
+        with open('players.txt', 'w') as f:
+            f.write(str(upd))
+
+def checkBalance(name):
+    with open('players.txt', 'r') as f:
+        data = f.read()
+        upd = ast.literal_eval(data)
+        return 'Your balance is: '+ str(upd[name])
+        
+def isEqual(playerPick):
+    side = ['h','t']
+    coinFlip = random.choice(side)
+    return playerPick == coinFlip
+
+def isNewPlayer(name):
+    with open('players.txt', 'r') as f: #opens players file to check if player already exists
+        data = f.read()
+        upd = ast.literal_eval(data)
+        return name not in upd
+
+def enoughBalance(name, amount):
+    with open('players.txt', 'r') as f: 
+        data = f.read()
+        upd = ast.literal_eval(data)
+        if int(upd[name]) >= amount:
+            return True
+        else:
+            return False
+
+
+#addPoints('nadjib', 100)
---
+++ main.py
@@ -0,0 +1,57 @@
+import discord, os
+from discord.ext import commands
+from gameFunctions import *
+
+client = discord.Client() #connection to discord
+
+@client.event #register an event
+async def on_ready():
+    print("Gamble Bot is online!")
+    await client.change_presence(activity=discord.Game(name="Made by Hemzyy. prefix is '$'"))
+
+
+@client.event #second event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$bet'): #Basically the whole point of this bot
+        msg = message.content.split(' ', 2)
+        arg = msg[1]
+        prediction = msg[2]
+        sender = message.author
+
+        if isNewPlayer(str(sender)):
+            addPlayer(str(sender))
+            await message.channel.send('You have been added to the players list with a balance of 50 points.')
+        else:
+            if (enoughBalance(str(sender), int(arg))):
+                removePoints(str(sender), int(arg))
+                if isEqual(str(prediction)):
+                    addPoints(str(sender), int(arg)*2)
+                    await message.channel.send('Congrats, you won double your bet!')
+                else:
+                    await message.channel.send('You lost.')
+            else:
+                await message.channel.send('You don\'t have enough points.')
+                
+    if message.content == ('$balance'):
+        sender = message.author
+        if isNewPlayer(str(sender)):
+            addPlayer(str(sender))
+            await message.channel.send('You have been added to the players list with a balance of 50 points.')
+        await message.channel.send('<@'+ str(message.author.id)+'>, '+ checkBalance(str(sender)))
+                    
+
+
+        
+    '''
+    if message.content == ("nik mok"):
+        await message.channel.send("ou yemak")
+
+    if message.content == "n7abek ghiles":
+        await message.channel.send("ok")
+    '''
+
+
+client.run('ODAwODMyNjgwMjQ5OTE3NDYy.YAX3jQ.Ayw_jmnisW7fuzA1Jy8zktCgpo0')
